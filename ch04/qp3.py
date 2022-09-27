490dd7ae8b27ba9e629410730219fece1104256e
shirakawanoseki <shirakawanoseki@users.noreply.github.com>
2022-09-27 16:14:30 +0900

    Add files via upload

---
+++ ch04/qp3.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cvxopt
+
+p = cvxopt.matrix(np.array([[2, 1], [1, 2]], dtype=np.float64))
+q = cvxopt.matrix(np.array([2, 4], dtype=np.float64))
+G = cvxopt.matrix(np.array([[2, 3]], dtype=np.float64))
+h = cvxopt.matrix(np.array([3], dtype=np.float64))
+
+sol = cvxopt.solvers.qp(p, q, G=G, h=h)
+
+print(np.array(sol["x"]))
+print(np.array(sol["primal objective"]))
+
